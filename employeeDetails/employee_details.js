b58bd31df39ba3e689b20bf86c17268317a8a7b5
TunahanCelik1 <ctunahan1@gmail.com>
2026-08-11 18:38:59 -0400

    message

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,7 +1,7 @@
 const employees = [
-      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization: 'Javascript' },
+      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization: 'Python' },
+      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization: 'Java' },
       //... Daha fazla çalışan kaydı burada eklenebilir
     ];
 // Tüm çalışanları görüntülemek için fonksiyon
@@ -32,3 +32,13 @@ function findEmployeeById(employeeId) {
         document.getElementById('employeesDetails').innerHTML = 'bu ID ile hiçbir çalışan bulunamadı';
        }
    }
+
+   function findBySpecialization(specialization) {
+    const foundSpec = employees.find(employee => employee.specialization === specialization);
+    
+    if (foundSpec) {
+        document.getElementById('employeesDetails').innerHTML = `<p>${foundSpec.id}: ${foundSpec.name} - ${foundSpec.department} - ${foundSpec.specialization}</p>`;
+    } else {
+        document.getElementById('employeesDetails').innerHTML = 'Bu uzmanlığa sahip çalışan bulunamadı.';
+    }
+}
