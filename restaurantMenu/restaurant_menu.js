7683e5d01db35b23011b47f46edb2451fd43a65b
TunahanCelik1 <ctunahan1@gmail.com>
2026-08-11 13:59:51 -0400

    message

--- restaurantMenu/restaurant_menu.js
+++ restaurantMenu/restaurant_menu.js
@@ -1,6 +1,6 @@
-const breakfastMenu = ['Pancakes', 'Eggs Benedict', 'Oatmeal', 'Frittata'];
-const mainCourseMenu = ['Steak', 'Pasta', 'Burger', 'Salmon'];
-const dessertMenu = ['Cake', 'Ice Cream', 'Pudding', 'Fruit Salad'];
+const breakfastMenu = ['Pankek - $12', 'Benedict Yumurtası - $22.99', 'Yulaf Ezmesi - $21.99', 'Frittata - $15'];
+const mainCourseMenu = ['Steak - $12', 'Pasta - $9', 'Burger - $5', 'Salmon - $12'];
+const dessertMenu = ['Cake - $16', 'Ice Cream - $10', 'Pudding - $4', 'Fruit Salad - $12'];
 
  const breakfastMenuItemsHTML = breakfastMenu.map((item, index) => `<p>Öğe ${index + 1}: ${item}</p>`).join('');
         document.getElementById('breakfastMenuItems').innerHTML = breakfastMenuItemsHTML;
